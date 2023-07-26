295d2c38a3fab3192a0b922a6e21f9be8e850b72
apghadi <139664592+apghadi@users.noreply.github.com>
2023-07-25 22:15:17 -0400

    Update streamlit_app.py
    added fruityvice api call

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,4 +23,6 @@ if not fruits_selected:
 else:
   streamlit.dataframe(fruits_to_show)
 
-
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response)
